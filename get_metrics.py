6e63439cd0f322eeea3e4b7604a15679539bdbf2
gladshire <miles.girard@gmail.com>
2025-04-11 19:30:35 -0500

    Added ipSAE score to analysis script

--- get_metrics.py
+++ get_metrics.py
@@ -1,5 +1,6 @@
 '''
-Code for extracting score metrics from AlphaFold3 runs
+(Dirty) Code for extracting score metrics from AlphaFold3 runs
+
 Written by Miles Woodcock-Girard for Drew Lab at UIC
 '''
 
@@ -7,29 +8,49 @@ import json
 import csv
 import os
 import sys
+import glob
+import subprocess
 
 import pdockq as pDockQ
 import uniprot_api
-import pae
-
+#import pae
+import utils
 
 
 with open("metrics.csv", 'w') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter = ",")
-    csv_writer.writerow(["Pair", "ipTM", "pTM", "Ranking Score", "pDockQ", "Disease Variant", "Novel Interaction"])
+    csv_writer.writerow(["Pair", "ipTM", "pTM", "Ranking Score", "pDockQ", "ipSAE", "Disease Variant", "Novel Interaction"])
 
     for i, pair in enumerate(os.listdir()):
         if os.path.isdir(pair) == False:
             continue
 
-        curr_json_scores = f"./{pair}/{pair}_confidences.json"
-        curr_json_summary = f"./{pair}/{pair}_summary_confidences.json"
-        curr_mmcif_path = f"./{pair}/{pair}_model.cif"
+        print(f"{i} {pair}")
 
-        if not os.path.exists(curr_json_path) or not os.path.exists(curr_mmcif_path):
+        if utils.usedAF3Server(pair):
+
+            if not glob.glob(f"./{pair}/fold_{pair}_*_summary_confidences_*.json"):
+                print(f"Empty directory: {pair} Skipping ...")
+                continue
+
+            best_model = utils.getBestModel(pair)
+
+            if not glob.glob(f"./{pair}/fold_{pair}_*_summary_confidences_{best_model}.json"):
+                print(f"Empty directory: {pair} Skipping ...")
+                continue
+
+            curr_json_scores = glob.glob(f"./{pair}/fold_{pair}_*_full_data_{best_model}.json")[0]
+            curr_json_summary = glob.glob(f"./{pair}/fold_{pair}_*_summary_confidences_{best_model}.json")[0]
+            curr_mmcif_path = glob.glob(f"./{pair}/fold_{pair}_*_model_{best_model}.cif")[0]
+
+        else:
+            curr_json_scores = f"./{pair}/{pair}_confidences.json"
+            curr_json_summary = f"./{pair}/{pair}_summary_confidences.json"
+            curr_mmcif_path = f"./{pair}/{pair}_model.cif"
+
+        if not os.path.exists(curr_json_summary) or not os.path.exists(curr_mmcif_path):
             continue
 
-        print(f"{i} {pair}")
 
         prot1 = pair.split("_")[0]
         prot2 = pair.split("_")[1]
@@ -44,7 +65,17 @@ with open("metrics.csv", 'w') as csv_file:
 
             # Obtain chain coordinates, pLDDTs from mmCIF file
             chain_coords, chain_plddt = pDockQ.read_model_file(curr_mmcif_path)
+
+            # Calculate pDockQ score for protein pair
             pdockq, ppv = pDockQ.calc_pdockq(chain_coords, chain_plddt, 8)
+
+            # Calculate ipSAE score for protein pair
+            ipsae_output = curr_mmcif_path[:-4:] + "_15_15.txt"
+            subprocess.run(["python3", "ipsae.py", curr_json_scores, curr_mmcif_path, str(15), str(15)])
+            with open(ipsae_output, 'r') as ipsae_file:
+                ipsae_data = ipsae_file.read().splitlines()[1:-1:]
+                ipsae_max = float(ipsae_data[3].split()[5])
+
 
             # Discern whether disease variants exist for protein
             numVars1 = uniprot_api.count_disease_var(prot1)
@@ -59,10 +90,10 @@ with open("metrics.csv", 'w') as csv_file:
             known_interact = uniprot_api.interaction_in_uniprot(prot1, prot2)
 
             # Get average PAE across all pairs of residues
-            avg_pae, pae_data, nres_c1, nres_c2 = pae.load_json(curr_json_scores)
+            #avg_pae, pae_data, nres_c1, nres_c2 = pae.load_json(curr_json_scores)
 
             # Write pair data to row in CSV output
-            csv_writer.writerow([pair, iptm, ptm, ranking_score, avg_pae, pdockq, disease_var, known_interact])
+            csv_writer.writerow([pair, iptm, ptm, ranking_score, pdockq, ipsae_max, disease_var, known_interact])
 
             curr_json_file.close()
 
---
+++ utils.py
@@ -0,0 +1,61 @@
+# General utilities for processing AlphaFold3 output
+# Written by Miles Woodcock-Girard for Drew Lab at UIC
+
+import os
+import glob
+import json
+import csv
+
+
+# Boolean function for determining whether AF3 model was generated using AF3 server
+def usedAF3Server(afOutputDir):
+
+    subdirs = [file for file in os.listdir(afOutputDir)
+               if os.path.isdir(os.path.join(afOutputDir, file))]
+
+    files = [file for file in os.listdir(afOutputDir)]
+
+    # AlphaFold3 server output has no subdirectories
+    if not subdirs:
+        return True
+    # AlphaFold3 local output has five subdirectories, one for each model (0 - 4)
+    elif len(subdirs) == 5:
+        return False
+    # Sanity check -- this case should never occur
+    else:
+        print("ERROR: This should not happen")
+        return False
+
+
+# Function to get the best-scoring AF3 model number (0 - 4), per their "ranking_score"
+# in their confidence summary JSON
+def getBestModel(afOutputDir):
+
+    usedServer = usedAF3Server(afOutputDir)
+
+    # AF3 webserver outputs have simpler directory structure
+    if usedServer:
+        summaryFiles = glob.glob(os.path.join(afOutputDir, "*_summary_confidences_*.json"))
+
+        ranking_scores = []
+        for summaryFile in summaryFiles:
+            with open(summaryFile, 'r') as summary_json:
+                json_data = json.load(summary_json)
+                ranking_scores.append(float(json_data["ranking_score"]))
+        model_number = ranking_scores.index(max(ranking_scores))
+
+        return model_number
+
+    else:
+        ranking_scores_csv = os.path.join(afOutputDir, "ranking_scores.csv")
+
+        ranking_scores = []
+        with open(ranking_scores_csv, 'r') as rankings_csv:
+            reader = csv.DictReader(rankings_csv)
+            for row in reader:
+                print(row)
+                ranking_scores.append(float(row["ranking_score"]))
+        model_number = ranking_scores.index(max(ranking_scores))
+
+        return model_number
+
